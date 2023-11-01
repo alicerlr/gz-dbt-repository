2732617a76e2c446b8e96b5610add2ec6a29885a
Alice Rieulier <alice.rieulier@gmail.com>
2023-11-01 14:15:55 +0000

    commit

--- models/  int_campaigns_day.sql
+++ models/  int_campaigns_day.sql
@@ -2,6 +2,9 @@ SELECT *
 FROM {{ref('stg_raw__adwords')}}
 UNION ALL 
 SELECT *
+FROM {{ref('stg_raw__bing')}}
+UNION ALL
+SELECT*
 FROM {{ref('stg_raw__criteo')}}
 UNION ALL
 SELECT*
---
+++ models/int_campaigns_day.sql
@@ -0,0 +1,14 @@
+WITH int_campaign_day AS (
+  SELECT
+    DATE_TRUNC(date, DAY) AS day, 
+    campaign_name,
+    SUM(ads_cost) AS total_ads_cost
+  FROM {{ ref('int_campaigns') }} 
+  GROUP BY 1, date_date
+)
+
+SELECT
+  day AS campaign_day,
+  campaign_name,
+  total_ads_cost
+FROM int_campaign_day
