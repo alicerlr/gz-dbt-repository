d0d5af430250758cda49f06161302137043f100d
Alice Rieulier <alice.rieulier@gmail.com>
2023-11-01 10:10:36 +0000

    commit finance_days

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,7 +1,12 @@
-SELECT *
-, (quantity * purchase_price) AS  purchase_cost
-, (revenue - quantity * purchase_price) AS margin
-from {{ref("stg_raw__sales")}}
-LEFT JOIN
-{{ref("stg_raw__product")}}
-USING (products_id)
+SELECT 
+    products_id, 
+    date_date,
+    orders_id,
+    revenue,
+    quantity,
+    purchase_price,
+    ROUND(CAST(s.quantity AS FLOAT64) * CAST(p.purchase_price AS FLOAT64), 2) AS purchase_cost,
+    s.revenue - ROUND(CAST(s.quantity AS FLOAT64) * CAST(p.purchase_price AS FLOAT64), 2) AS margin
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p
+        USING (products_id)
